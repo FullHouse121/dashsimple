c63e648d4b87c76e94e0118412886d70c57dcee6
FullHouse121 <fullhouseclub1@gmail.com>
2026-02-13 13:14:36 +0400

    Expand ARPPU growth timeline width

--- src/App.jsx
+++ src/App.jsx
@@ -2833,7 +2833,7 @@ function GeosDashboard({ filters }) {
             </motion.div>
 
             <motion.div
-              className="panel span-2"
+              className="panel span-3"
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.2 }}
